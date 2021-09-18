a1048a1ec61b581747b689daf274d72dc2505527
Niloy Mazumder <neeloy.mazumder29@gmail.com>
2021-09-18 20:08:07 +0530

    Logout Test Executed Successfully

--- src/main/java/AnydoTest/LogoutTest.java
+++ src/main/java/AnydoTest/LogoutTest.java
@@ -13,21 +13,26 @@ public class LogoutTest {
         System.setProperty("webdriver.chrome.driver", "/Users/niloymazumder/Documents/IdeaProjects/WebDriver/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(6,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(8,TimeUnit.SECONDS);
 
         driver.get("https://www.any.do/");
 
+//        Login Process
         driver.findElement(By.xpath("//a[contains(@class,'-secondary')]")).click();
         driver.findElement(By.xpath("//div[contains(text(),'Continue with Email or Apple')]")).click();
         driver.findElement(By.xpath("//input[contains(@placeholder,'your@email.com')]")).sendKeys("neeloy.mazumder29@gmail.com");
         driver.findElement(By.xpath("//button[contains(@type,'submit')]")).click();
-        Thread.sleep(4000);
+
 
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("zizky6-quhhed-Xebtem");
         driver.findElement(By.xpath("//button[contains(@type,'submit')]")).click();
-        Thread.sleep(2000);
 
+
+//        Logout Process
+        Thread.sleep(4000);
         driver.findElement(By.xpath("//button[@aria-label='Settings']")).click();
+        driver.findElement(By.xpath("//div[normalize-space()='My Profile']")).click();
+        driver.findElement(By.xpath("//div[normalize-space()='Sign Out']")).click();
 
 
     }
